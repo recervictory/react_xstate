1b36b942239e293390f3711c90a39b63b39652a3
Victor Banerjee <recervictory@gmail.com>
2023-02-08 17:22:11 +0530

    Building First Machine

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,8 +1,27 @@
+import { useMachine } from "@xstate/react";
+import { testMachine } from "./machines/testMachine";
+
+
 
 function App() {
+  const [state, send] = useMachine(testMachine)
+
   return (
     <div className="App">
       <p>React Xstate</p>
+      <div>
+        {JSON.stringify(state.value)}
+      </div>
+      <button
+        onClick={ () => send("MOUSEOVER")}      
+      >
+        MOUSEOVER
+      </button>
+      <button
+        onClick={ () => send("MOUSEOUT")}      
+      >
+        MOUSEOUT
+      </button>
     </div>
   );
 }
